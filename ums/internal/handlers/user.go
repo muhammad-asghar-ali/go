a592612f7efa7ce4c11fc3937d8ae5799c777e88
asgr <51094535+muhammad-asghar-ali@users.noreply.github.com>
2024-08-20 10:28:48 -0700

    chore(): ums handlers (#4)
    * feat() initial commit and setup ums
    
    * chore(): ums handlers

--- ums/internal/handlers/user.go
+++ ums/internal/handlers/user.go
@@ -1,34 +1,94 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+	"ums/internal/models"
 
 	"github.com/julienschmidt/httprouter"
-	"gopkg.in/mgo.v2"
 )
 
 type (
 	UserHandler struct {
-		s *mgo.Session
+		svc *models.Svc
 	}
 )
 
-func NewUserHandler(s *mgo.Session) *UserHandler {
-	return &UserHandler{s}
+// NewUserHandler creates a new UserHandler with the given service
+func NewUserHandler(us *models.Svc) *UserHandler {
+	return &UserHandler{svc: us}
 }
 
-func (uh *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func (uh *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	user := models.User{}
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
+	if err := uh.svc.CreateUser(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
 }
 
 func (uh *UserHandler) GetUserByID(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	id := ps.ByName("id")
 
+	user, err := uh.svc.GetUserByID(id)
+	if err != nil {
+		if err.Error() == "invalid ID format" {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		} else {
+			http.Error(w, "User not found", http.StatusNotFound)
+		}
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(user)
 }
 
 func (uh *UserHandler) UpdateUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	id := ps.ByName("id")
 
+	user := models.User{}
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := uh.svc.UpdateUser(id, &user); err != nil {
+		if err.Error() == "invalid ID format" {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		} else {
+			http.Error(w, "User not found", http.StatusNotFound)
+		}
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(user)
 }
 
 func (uh *UserHandler) DeleteUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	id := ps.ByName("id")
 
+	if err := uh.svc.DeleteUser(id); err != nil {
+		if err.Error() == "invalid ID format" {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		} else {
+			http.Error(w, "User not found", http.StatusNotFound)
+		}
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("User deleted successfully"))
 }
--- ums/internal/models/user.go
+++ ums/internal/models/user.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"errors"
+
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -11,4 +14,64 @@ type (
 		Gender string        `bson:"gender" json:"gender"`
 		Age    int           `bson:"age" json:"age"`
 	}
+
+	Svc struct {
+		session *mgo.Session
+	}
 )
+
+// NewSvc creates a new service with the provided session
+func NewSvc(session *mgo.Session) *Svc {
+	return &Svc{session: session}
+}
+
+func (us *Svc) CreateUser(user *User) error {
+	user.ID = bson.NewObjectId()
+
+	session := us.session.Clone()
+	defer session.Close()
+
+	c := session.DB("ums").C("users")
+	return c.Insert(user)
+}
+
+func (us *Svc) GetUserByID(id string) (*User, error) {
+	if !bson.IsObjectIdHex(id) {
+		return nil, errors.New("invalid ID format")
+	}
+
+	session := us.session.Clone()
+	defer session.Close()
+
+	c := session.DB("ums").C("users")
+	user := &User{}
+	if err := c.FindId(bson.ObjectIdHex(id)).One(user); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (us *Svc) UpdateUser(id string, updatedUser *User) error {
+	if !bson.IsObjectIdHex(id) {
+		return errors.New("invalid ID format")
+	}
+
+	session := us.session.Clone()
+	defer session.Close()
+
+	c := session.DB("ums").C("users")
+	return c.UpdateId(bson.ObjectIdHex(id), updatedUser)
+}
+
+func (us *Svc) DeleteUser(id string) error {
+	if !bson.IsObjectIdHex(id) {
+		return errors.New("invalid ID format")
+	}
+
+	session := us.session.Clone()
+	defer session.Close()
+
+	c := session.DB("ums").C("users")
+	return c.RemoveId(bson.ObjectIdHex(id))
+}
